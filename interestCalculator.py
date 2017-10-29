06b5447c2d58ed2faa2505d8b5ba2761cd949e5f
Jake <priddyjacob84@gmail.com>
2017-10-29 15:04:26 -0400

    created intrest calculator

---
+++ interestCalculator.py
@@ -0,0 +1,12 @@
+#Calculate savings of $100 with 10% interest over 7 years
+
+#Create savings
+savings = 100
+#Create factor
+factor = 1.10
+#Create duration (in years)
+years = 7
+#Find the result of savings with interest over 7 years
+result = savings * factor ** years
+#Print result
+print(result)
